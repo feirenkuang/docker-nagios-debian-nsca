9212f86a7f566fdeb79100dc30d0af852d8470e2
feirenkuang <49110779+feirenkuang@users.noreply.github.com>
2020-03-07 13:06:44 +0800

    Update setup-on-run.sh

--- setup-on-run.sh
+++ setup-on-run.sh
@@ -4,5 +4,5 @@
 htpasswd -c -b -s /etc/nagios3/htpasswd.users ${NAGIOSADMIN_USER} ${NAGIOSADMIN_PASS}
 chown nagios:nagios /etc/nagios3/htpasswd.users
 
-sed -i "s,nagiosadmin,${NAGIOSADMIN_USER}," /etc/nagios3/cgi.cfg
+sed -i "s,jf,${NAGIOSADMIN_USER}," /etc/nagios3/cgi.cfg
 
